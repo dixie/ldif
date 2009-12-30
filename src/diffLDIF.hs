4c7224fc0d85a858f6c92badf6cbfcb18ec23662
dixiecko <dixiecko@gmail.com>
2009-12-30 23:11:19 +0000

    Implementation of LDIF serialization back to String.

--- src/diffLDIF.hs
+++ src/diffLDIF.hs
@@ -1,6 +1,7 @@
 -- | Make Change LDIF based on two Content LDIFs
 import Data.List
 import Data.Either
+import Data.Maybe
 import Control.Monad 
 import System.FilePath 
 import System.Environment
@@ -10,8 +11,6 @@ main = do
   args <- getArgs
   ml1 <- parseLDIFFile (args !! 0)
   ml2 <- parseLDIFFile (args !! 1)
-  print ml1
-  print ml2 
   case rights [ml1,ml2] of 
-       [l1,l2] -> print $ diffLDIF l1 l2  
+       [l1,l2] -> putStrLn $ ldif2Str $ fromJust $ diffLDIF l1 l2  
        _       -> print $ lefts [ml1,ml2] 
