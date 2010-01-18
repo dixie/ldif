b6f192ac6af80dcd5af282dca7f211b53fae3c4a
dixiecko <dixiecko@gmail.com>
2010-01-18 13:41:52 +0000

    Implement better diplaying for HTML output.

--- src/ldif2html.hs
+++ src/ldif2html.hs
@@ -6,10 +6,11 @@ import Control.Monad
 import System.FilePath 
 import System.Environment
 import Text.LDIF
+import qualified Data.Set as Set
 
-ldif2html :: LDIF -> String
-ldif2html (LDIFContent v xs) = unlines $ (ver2Str v) ++ (map (content2html) xs)
-ldif2html (LDIFChanges v xs) = unlines $ (ver2Str v) ++ (map (change2html) xs)
+ldif2html :: Set.Set String -> LDIF -> String
+ldif2html idx (LDIFContent v xs) = unlines $ (ver2Str v) ++ (map (content2html idx) xs)
+ldif2html idx (LDIFChanges v xs) = unlines $ (ver2Str v) ++ (map (change2html idx) xs)
 
 -- | Serialize version to LDIF Format Lines
 ver2Str :: Maybe String -> [String]
@@ -18,41 +19,61 @@ ver2Str (Just v) = ["version: "++v]
 
 -- | Serialize DN to LDIF Format
 dn2html :: DN -> String
-dn2html (DN xs) = concat [ "<div id=\"abc"++nm++"\"></div><b>dn:</b> <font color=\"green\"><b>", intercalate "," $ map (\(n,v) -> n++"="++v) xs, "</b></font>"]
+dn2html ys@(DN xs) = concat [ "<div id=\"abc"++nm++"\"></div><b>dn:</b> <font color=\"green\"><b>", intercalate "," $ map (\(n,v) -> n++"="++v) xs, "</b></font>"]
       where
-         nm = snd $ head xs
+         nm = dn2last ys
+
+dn2last (DN xs) = snd $ head xs
 
 -- | Serialize Content Record in LDIF Format
-content2html :: ContentRecord -> String
-content2html (ContentRecord dn xs) = unlines $ [ (dn2html dn) ] ++ (attrVals2Ln xs)
+content2html :: Set.Set String -> ContentRecord -> String
+content2html idx (ContentRecord dn xs) = unlines $ [ (dn2html dn) ] ++ (attrVals2Ln idx xs)
 
 -- | Serialize Change Record in LDIF Format
-change2html :: ChangeRecord -> String
-change2html (ChangeRecord dn (ChangeDelete))     = unlines   [ (dn2html dn), "changetype: delete" ]
-change2html (ChangeRecord dn (ChangeAdd xs))     = unlines $ [ (dn2html dn), "changetype: add"    ] ++ (attrVals2Ln xs)
-change2html (ChangeRecord dn (ChangeModify xs))  = unlines $ [ (dn2html dn), "changetype: modify" ] ++ (mods2Ln xs)
-change2html (ChangeRecord dn (ChangeModDN))      = unlines $ [ (dn2html dn), "changetype: moddn"  ]
+change2html :: Set.Set String -> ChangeRecord -> String
+change2html idx (ChangeRecord dn (ChangeDelete))     = unlines   [ (dn2Str dn), "changetype: delete" ]
+change2html idx (ChangeRecord dn (ChangeAdd xs))    = unlines $ [ (dn2html dn), "changetype: add"    ] ++ (attrVals2Ln idx xs)
+change2html idx (ChangeRecord dn (ChangeModify xs))  = unlines $ [ (dn2Str dn), "changetype: modify" ] ++ (mods2Ln idx xs)
+change2html idx (ChangeRecord dn (ChangeModDN))      = unlines $ [ (dn2Str dn), "changetype: moddn"  ]
 
-attrVals2Ln :: [AttrValue] -> [String]
-attrVals2Ln xs = map (attrVal2Ln) xs
+attrVals2Ln :: Set.Set String -> [AttrValue] -> [String]
+attrVals2Ln idx xs = map (attrVal2Ln idx) xs
 
-attrVal2Ln :: AttrValue -> String
-attrVal2Ln (n,v) = "<b>"++n++"</b> : <a href=\"#abc"++v++"\">"++v++"</a>"
+attrVal2Ln :: Set.Set String -> AttrValue -> String
+attrVal2Ln idx (n,v) = if Set.member v idx then "<b>"++n++"</b> : <a href=\"#abc"++v++"\">"++v++"</a>"
+                          else "<b>"++n++"</b> : "++v
 
-mods2Ln :: [Modify] -> [String]
-mods2Ln xs = intercalate ["-"] $ map (mod2Ln) xs
+mods2Ln :: Set.Set String -> [Modify] -> [String]
+mods2Ln idx xs = intercalate ["-"] $ map (mod2Ln idx) xs
 
-mod2Ln :: Modify -> [String]
-mod2Ln (ModAdd     nm xs) = [ attrVal2Ln ("add",nm)     ] ++ (map (\v -> attrVal2Ln (nm,v)) xs) 
-mod2Ln (ModDelete  nm xs) = [ attrVal2Ln ("delete",nm)  ] ++ (map (\v -> attrVal2Ln (nm,v)) xs)
-mod2Ln (ModReplace nm xs) = [ attrVal2Ln ("replace",nm) ] ++ (map (\v -> attrVal2Ln (nm,v)) xs)
+mod2Ln :: Set.Set String -> Modify -> [String]
+mod2Ln idx (ModAdd     nm xs) = [ attrVal2Ln idx ("add",nm)     ] ++ (map (\v -> attrVal2Ln idx (nm,v)) xs) 
+mod2Ln idx (ModDelete  nm xs) = [ attrVal2Ln idx ("delete",nm)  ] ++ (map (\v -> attrVal2Ln idx (nm,v)) xs)
+mod2Ln idx (ModReplace nm xs) = [ attrVal2Ln idx ("replace",nm) ] ++ (map (\v -> attrVal2Ln idx (nm,v)) xs)
 
 -- Dummy ldif2html implementation but it should
 -- display LDIF file as HTML with values as href to
 -- DN which has the value as the leaf RDN value
 main = do
   args <- getArgs
-  ml <- parseLDIFFile (args !! 0)
-  case ml of 
-       Right l -> putStrLn $ concat ["<html><body bgcolor=lightyellow><pre>", ldif2html $ l, "</pre></body></html>" ]
-       Left e  -> print e
+  case args of 
+     []         -> putStrLn "Usage: ldif2html <input.ldif> [<input2.ldif> <input3.ldif> ... <inputN.ldif> <output.ldif>]"
+     [inp]      -> do
+                      doLDIF2HTML [inp] (putStrLn)
+                      putStrLn "# Done."
+     xs         -> do
+                      doLDIF2HTML (init xs) (writeFile (last xs)) 
+                      putStrLn $ "## Processed : " ++ (unlines $ init xs)
+                      putStrLn $ "## Written   : " ++ (last xs)
+
+doLDIF2HTML names outF = do
+  mls <- mapM (parseLDIFFile) names
+  case lefts mls of 
+       []   -> do
+                 let idx = Set.unions $ map (dns) (rights mls)
+                 outF $ concat ["<html><body bgcolor=lightyellow><pre>", concat $ map (ldif2html idx) (rights mls), "</pre></body></html>" ]
+       xs  -> mapM_ print xs
+
+dns :: LDIF -> Set.Set String
+dns (LDIFContent _ xs) = Set.fromList $ nub $ map (dn2last . coDN) xs
+dns (LDIFChanges _ xs) = Set.fromList $ nub $ map (dn2last . chDN) xs
