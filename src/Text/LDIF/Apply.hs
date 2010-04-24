3ed91476e4530bf622ba0a2fe8b3b95838515196
dixiecko <dixiecko@gmail.com>
2010-04-24 11:37:07 +0000

    Update the error reporting.

--- src/Text/LDIF/Apply.hs
+++ src/Text/LDIF/Apply.hs
@@ -24,10 +24,10 @@ applyRecord2LDIF rec@(ChangeRecord  dn op)   dst = applyChange2Record op dn dst 
 -- | Apply one LDIF Change (add/del/modf) for given DN within LDIF Content 
 applyChange2Record :: Change -> DN -> LDIF -> Maybe LDIFRecord -> LDIF
 applyChange2Record (ChangeAdd vals)   dn (LDIFContent v xs) Nothing  = LDIFContent v (xs++[ContentRecord dn vals]) 
-applyChange2Record (ChangeAdd vals)   dn (LDIFContent v xs) (Just _) = error "already exists"
+applyChange2Record (ChangeAdd vals)   dn (LDIFContent v xs) (Just _) = error ("ADD: Already exists: "++(show dn))
 applyChange2Record ChangeDelete       dn (LDIFContent v xs) (Just _) = let rn = filter (\x -> dn /= (reDN x)) xs
                                                                        in LDIFContent v rn
-applyChange2Record ChangeDelete       dn (LDIFContent v xs) Nothing  = error "not found"
+applyChange2Record ChangeDelete       dn (LDIFContent v xs) Nothing  = error ("DELETE: Entry not found: "++(show dn))
 applyChange2Record (ChangeModify ops) dn (LDIFContent v xs) (Just r) = let pre  = takeWhile (\x -> dn /= (reDN x)) xs
                                                                            post = filter (\x -> dn /= (reDN x)) $ dropWhile (\x -> dn /= (reDN x)) xs
                                                                            rn   = foldr applyMod2Record r ops
@@ -39,15 +39,15 @@ applyMod2Record :: Modify -> LDIFRecord -> LDIFRecord
 applyMod2Record (ModAdd      name vals) (ContentRecord dn av) = let nav = map (\v -> (name,v)) vals  -- new attr/values
                                                                     mav = av ++ nav                  -- merged attr/values
                                                                     verified = if (length $ nub mav) == (length mav) then mav
-                                                                               else error "ModAdd: Values already exists"
+                                                                               else error ("ModAdd: Values already exists: "++(show av)++" vs "++(show vals)++" DN:"++(show dn))
                                                                     in ContentRecord dn verified
 applyMod2Record (ModDelete   name [])   (ContentRecord dn av) = let nav = filter (\(n,v) -> n /= name) av -- new attr/values
                                                                     verified = if (length $ nub nav) /= (length av) then nav
-                                                                               else error "ModDel: Attribute not found"
+                                                                               else error ("ModDel: Attribute not found: "++(show name)++" DN:"++(show dn))
                                                                 in ContentRecord dn verified
 applyMod2Record (ModDelete   name vals) (ContentRecord dn av) = let mav = filter (\(n,v) -> n /= name || v `notElem` vals) av
                                                                     verified = if (length av) - (length mav) == (length vals) then mav
-                                                                               else error "ModDel: Attribute/Value not found"
+                                                                               else error ("ModDel: Attribute/Value not found: "++(show name)++"vals"++(show vals)++" DN:"++(show dn))
                                                                 in ContentRecord dn verified
 applyMod2Record (ModReplace  name vals) (ContentRecord dn av) = let nav = map (\v -> (name,v)) vals -- new attr/values
                                                                     mav = (filter (\(n,v) -> n /= name) av) ++ nav -- merged
