5860396a3ed2a40a1e7abb1cd3a2310841574b8d
dixiecko <dixiecko@gmail.com>
2010-05-15 11:36:06 +0000

    Use the cmdArgs in diffLDIF command.

--- src/diffLDIF.hs
+++ src/diffLDIF.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 -- | Make Change LDIF based on two Content LDIFs
 import Data.List
 import Data.Either
@@ -6,11 +7,24 @@ import Control.Monad
 import System.FilePath 
 import System.Environment
 import Text.LDIF
+import System.Console.CmdArgs
+
+data DiffLdif = DiffLdif { srcFile :: FilePath
+                         , dstFile :: FilePath } deriving (Show, Data, Typeable)
+
+defaultCfg = mode $ DiffLdif { srcFile = def &= typFile & flag "s" & text "Source LDIF File"
+                             , dstFile = def &= typFile & flag "t" & text "Target LDIF File" }
+
+verifyCfg :: DiffLdif -> IO ()
+verifyCfg (DiffLdif [] _) = error "Missing Source LDIF File parameter (-s)"
+verifyCfg (DiffLdif _ []) = error "Missing Target LDIF File parameter (-t)"
+verifyCfg (DiffLdif _ _ ) = return ()
 
 main = do
-  args <- getArgs
-  ml1 <- parseLDIFFile (args !! 0)
-  ml2 <- parseLDIFFile (args !! 1)
+  cfg <- cmdArgs "LDIF Diff. Create delta LDIF between Source LDIF and Target LDIF" [defaultCfg]
+  verifyCfg cfg
+  ml1 <- parseLDIFFile (srcFile cfg)
+  ml2 <- parseLDIFFile (dstFile cfg)
   case rights [ml1,ml2] of 
        [l1,l2] -> case diffLDIF l1 l2 of
                     Left err -> putStrLn err
