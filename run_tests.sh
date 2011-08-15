8d344c7cfc776ca21a4d80c1c155a82467b08237
Radoslav Dorcik <dixiecko@gmail.com>
2011-08-15 11:25:46 +0000

    Add more options for module test launcher.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-cabal configure -ftest && cabal build && cd tests && ../dist/build/test/test
+function showUsage()
+{
+   echo "Usage: $1 [ build | rebuild | run ]"
+}
+
+if [ -z "$1" ]; then
+   showUsage $0
+   exit 1;
+fi
+
+if [ "$1" = "run" ]; then
+    ./dist/build/test/test
+elif [ "$1" = "build" ]; then
+    cabal configure -ftest && cabal build && ./dist/build/test/test
+elif [ "$1" = "rebuild" ]; then
+    cabal clean
+    cabal configure -ftest && cabal build && ./dist/build/test/test
+else
+    showUsage $0
+fi
