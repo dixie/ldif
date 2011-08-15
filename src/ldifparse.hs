cf3d3c8fea7b9b3c6af1418612d653d9258adfc7
Radoslav Dorcik <dixiecko@gmail.com>
2011-08-15 10:55:06 +0000

    Align test parser command for new LDIF Interface.

--- src/ldifparse.hs
+++ src/ldifparse.hs
@@ -10,10 +10,8 @@ import qualified Data.ByteString.Char8 as BC
 
 main = do
   args <- getArgs
-  xs <- BC.readFile  (args !! 0)
-  print $ BC.length xs
-  let ml1 = parseLDIFStr xs
+  let name = args !! 0
+  ml1 <- parseLDIFFile defaulLDIFConf name
   case ml1 of 
-       Right l1 -> do
-             BC.putStrLn $ ldif2str l1
+       Right l1 -> BC.putStrLn $ ldif2str l1
        Left err -> print err
